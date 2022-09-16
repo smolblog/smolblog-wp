75a2f5823a88b0cf39ce601439d794bd5033a2fb
Evan Hildreth <me@eph.me>
2022-09-15 21:23:29 -0400

    Start trying the new thing

--- smolblog-wp.php
+++ smolblog-wp.php
@@ -35,6 +35,8 @@ use Smolblog\OAuth2\Client\Provider\Twitter as TwitterOAuth;
 add_action(
 	'plugins_loaded',
 	function() {
+		global $smolblog_app;
+
 		// Load environment variables.
 		$environment = new Environment( apiBase: 'https://smolbeta.localhost/wp-json/smolblog/v2' );
 
@@ -52,23 +54,13 @@ add_action(
 		$app->container->extend( ConnectionCredentialFactory::class )->addArgument( Connection_Credential_Helper::class );
 		$app->container->extend( TransientFactory::class )->addArgument( Transient_Helper::class );
 
-		// Create the Twitter connector and register it.
-		$twitter_connector = new TwitterConnector(
-			provider: new TwitterOAuth(
-				array(
-					'clientId'     => 'mememe',
-					'clientSecret' => 'youyou',
-				)
-			),
-			factory: $app->container->get( ConnectionCredentialFactory::class )
-		);
-		$app->container->get( ConnectorRegistrar::class )->register( $twitter_connector );
-
 		// Start Smolblog.
 		$app->startup();
 
 		// Register endpoints with WordPress.
 		add_action( 'rest_api_init', array( $endpoint_registrar, 'init_endpoints' ) );
+
+		$smolblog_app = $app;
 	}
 );
 
@@ -79,8 +71,11 @@ add_action(
 			'smolblog-test',
 			'Smolblog',
 			function() {
+				global $smolblog_app;
 				?>
 			<a href="<?php echo esc_attr( get_rest_url( null, 'smolblog/v2/connect/init/twitter' ) ); ?>?_wpnonce=<?php echo esc_attr( wp_create_nonce( 'wp_rest' ) ); ?>" class="button">Sign in with Twitter</a>
+
+			<pre><?php print_r( $smolblog_app ); ?></pre>
 				<?php
 			}
 		);
